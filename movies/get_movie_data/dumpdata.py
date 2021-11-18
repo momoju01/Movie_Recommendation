fa5aa1b173e5adc30c91148793fd179e636ff173
김윤하 <beupthere@gmail.com>
2021-11-18 11:26:25 +0900

    1118 views.py 에 genres 수정

--- movies/get_movie_data/dumpdata.py
+++ movies/get_movie_data/dumpdata.py
@@ -1,9 +1,11 @@
-# 영화정보를 API를 이용해서 가져오기
+# # 영화정보를 API를 이용해서 가져오기
 import json
 import requests
 # from api_key import URLMaker
 from decouple import config
 MOVIE_API_KEY = config('API_KEY')
+
+
 
 # 1. movie 정보
 result = []
@@ -31,92 +33,21 @@ for page in range(1, 6):
         }
         result.append(movie_dict)
 
+
+
 with open('movies.json', 'w', encoding='UTF-8') as file:
     file.write(json.dumps(result, ensure_ascii=False))
 
 
 ## 2. genre 정보
-
-data = [
-    {
-      "id": 28,
-      "name": "액션"
-    },
-    {
-      "id": 12,
-      "name": "모험"
-    },
-    {
-      "id": 16,
-      "name": "애니메이션"
-    },
-    {
-      "id": 35,
-      "name": "코미디"
-    },
-    {
-      "id": 80,
-      "name": "범죄"
-    },
-    {
-      "id": 99,
-      "name": "다큐멘터리"
-    },
-    {
-      "id": 18,
-      "name": "드라마"
-    },
-    {
-      "id": 10751,
-      "name": "가족"
-    },
-    {
-      "id": 14,
-      "name": "판타지"
-    },
-    {
-      "id": 36,
-      "name": "역사"
-    },
-    {
-      "id": 27,
-      "name": "공포"
-    },
-    {
-      "id": 10402,
-      "name": "음악"
-    },
-    {
-      "id": 9648,
-      "name": "미스터리"
-    },
-    {
-      "id": 10749,
-      "name": "로맨스"
-    },
-    {
-      "id": 878,
-      "name": "SF"
-    },
-    {
-      "id": 10770,
-      "name": "TV 영화"
-    },
-    {
-      "id": 53,
-      "name": "스릴러"
-    },
-    {
-      "id": 10752,
-      "name": "전쟁"
-    },
-    {
-      "id": 37,
-      "name": "서부"
-    }
-]
-
 result = []
+genre_url = 'https://api.themoviedb.org/3/genre/movie/list'
+key = MOVIE_API_KEY
+GENRE_URL = f'{genre_url}?api_key={key}&language=ko-Kr'
+# https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>&language=en-US
+
+raw_data = requests.get(GENRE_URL).json()
+data = raw_data.get('genres')
 
 for genre in data:
     genre_dict = {
